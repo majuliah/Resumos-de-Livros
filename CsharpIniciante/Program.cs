b058b0ebb4100b5d888c8ded9147910ff9b9173f
Majulia <majulialcarvalho@gmail.com>
2021-10-17 21:19:42 -0300

    prática do livro

---
+++ CsharpIniciante/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CsharpIniciante
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int numero = int.Parse(Console.ReadLine());
+            Console.WriteLine(numero);
+        }
+    }
+}
